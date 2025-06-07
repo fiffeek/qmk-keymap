aa641cab6e9a777e580836cfc7d8910a7193d85e
Filip Mikina <filipmikina@gmail.com>
2025-06-08 00:52:10 +0200

    add assertions for led counts

--- scripts/colors.py
+++ scripts/colors.py
@@ -36,8 +36,11 @@ class Hand(Enum):
 
 
 class LedLayer:
-    left: List[LedColor]
-    right: List[LedColor]
+    left: List[List[LedColor]]
+    right: List[List[LedColor]]
+    REGULAR_ROW_COUNT = 4
+    REGULAR_ROW_LED_COUNT = 6
+    THUMB_ROW_LED_COUNT = 2
 
     def __init__(self) -> None:
         self.left = []
@@ -46,10 +49,18 @@ class LedLayer:
     def add_row(self, hand: Hand, leds: List[List[LedColor]]):
         led_list = flatten(leds)
         piece = self.left if hand == Hand.LEFT else self.right
-        piece.extend(led_list)
+        expected_length = (
+            self.REGULAR_ROW_LED_COUNT
+            if len(piece) < self.REGULAR_ROW_COUNT
+            else self.THUMB_ROW_LED_COUNT
+        )
+        assert (
+            len(led_list) == expected_length
+        ), f"len is {len(led_list)} but expected is {expected_length}"
+        piece.append(led_list)
 
     def to_list(self):
-        return flatten([self.left, self.right])
+        return flatten(flatten([self.left, self.right]))
 
 
 def find_ledmap_start(filepath: str):
